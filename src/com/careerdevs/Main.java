39335dc1a0974934ce948acab2171d24da4afdad
Manny Lee <many221@gmail.com>
2022-06-07 22:09:48 -0700

    finished panel walthrough

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -10,31 +10,68 @@ public class Main {
 //	 write your code here
 //
         ImageIcon image = new ImageIcon ("src/com/careerdevs/back.jpg");
-        Border border = BorderFactory.createLineBorder ( Color.LIGHT_GRAY,3 );
+      //  Border border = BorderFactory.createLineBorder ( Color.LIGHT_GRAY,3 );
 
-        JLabel label = new JLabel ();
-        label.setText ( "Coding My Is Fun 🙃");
-        label.setIcon ( image );
-        label.setHorizontalTextPosition ( JLabel.CENTER);
-        label.setVerticalTextPosition ( JLabel.TOP );
-        label.setForeground ( Color.WHITE );
-        label.setFont ( new Font ( "Helvetica",Font.BOLD,20) );
-        label.setIconTextGap ( -330 );
-        label.setBackground ( Color.BLACK );
-        label.setOpaque ( true );
-        label.setBorder ( border );
-        label.setVerticalAlignment ( JLabel.CENTER );
-        label.setHorizontalAlignment ( JLabel.CENTER );
+      //JLabel
+//        JLabel label = new JLabel ();
+//        label.setText ( "Coding My Is Fun 🙃");
+//        label.setIcon ( image );
+//        label.setHorizontalTextPosition ( JLabel.CENTER);
+//        label.setVerticalTextPosition ( JLabel.TOP );
+//        label.setForeground ( Color.WHITE );
+//        label.setFont ( new Font ( "Helvetica",Font.BOLD,20) );
+//        label.setIconTextGap ( -330 );
+//        label.setBackground ( Color.BLACK );
+//        label.setOpaque ( true );
+//        label.setBorder ( border );
+//        label.setVerticalAlignment ( JLabel.CENTER );
+//        label.setHorizontalAlignment ( JLabel.CENTER );
        // label.setBounds ( 0,0,250,250 );
 //        label.setVisible ( true );
 
+        JLabel blackLabel = new JLabel ("Black");
+        blackLabel.setForeground ( Color.WHITE );
+
+        JLabel redLabel = new JLabel ("Red");
+        redLabel.setForeground ( Color.blue );
+
+        JLabel greenLabel = new JLabel ("Green");
+        greenLabel.setForeground ( Color.MAGENTA );
+        greenLabel.setVerticalAlignment ( JLabel.TOP );
+        greenLabel.setHorizontalAlignment ( JLabel.CENTER );
+
+
+        //JPanel
+        JPanel blackPanel = new JPanel ();
+        blackPanel.setBackground ( Color.BLACK );
+        blackPanel.setBounds ( 0,0,250,250 );
+
+        JPanel redPanel = new JPanel ();
+        redPanel.setBackground ( Color.RED);
+        redPanel.setBounds ( 250,0,250,250 );
+
+        JPanel greenPanel = new JPanel ();
+        greenPanel.setBackground ( Color.GREEN);
+        greenPanel.setBounds ( 0,250,500,250 );
+        greenPanel.setLayout ( new BorderLayout () );
+
+        //JFrame
         JFrame frame = new JFrame ();
         frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
-        //frame.setSize ( 600,600 );
-        //frame.setLayout ( null );
+        frame.setTitle ( "Java GUI Test" );
+        frame.setSize ( 600,600 );
+        frame.setLayout ( null );
         frame.setVisible ( true );
-        frame.add(label);
-        frame.pack ();
+      //  frame.add(label);
+//        frame.pack ();
+
+        blackPanel.add ( blackLabel );
+        redPanel.add ( redLabel );
+        greenPanel.add ( greenLabel );
+
+        frame.add ( blackPanel );
+        frame.add ( redPanel );
+        frame.add ( greenPanel );
         frame.revalidate ();
 
 
