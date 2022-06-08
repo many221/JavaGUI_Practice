7bc3a2c83d6c143e142cbbcd727fb0f8b7ad06be
Manny Lee <many221@gmail.com>
2022-06-07 22:39:32 -0700

    finished button walthrough

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -29,52 +29,53 @@ public class Main {
        // label.setBounds ( 0,0,250,250 );
 //        label.setVisible ( true );
 
-        JLabel blackLabel = new JLabel ("Black");
-        blackLabel.setForeground ( Color.WHITE );
-
-        JLabel redLabel = new JLabel ("Red");
-        redLabel.setForeground ( Color.blue );
-
-        JLabel greenLabel = new JLabel ("Green");
-        greenLabel.setForeground ( Color.MAGENTA );
-        greenLabel.setVerticalAlignment ( JLabel.TOP );
-        greenLabel.setHorizontalAlignment ( JLabel.CENTER );
-
-
-        //JPanel
-        JPanel blackPanel = new JPanel ();
-        blackPanel.setBackground ( Color.BLACK );
-        blackPanel.setBounds ( 0,0,250,250 );
-
-        JPanel redPanel = new JPanel ();
-        redPanel.setBackground ( Color.RED);
-        redPanel.setBounds ( 250,0,250,250 );
-
-        JPanel greenPanel = new JPanel ();
-        greenPanel.setBackground ( Color.GREEN);
-        greenPanel.setBounds ( 0,250,500,250 );
-        greenPanel.setLayout ( new BorderLayout () );
+//        JLabel blackLabel = new JLabel ("Black");
+//        blackLabel.setForeground ( Color.WHITE );
+//
+//        JLabel redLabel = new JLabel ("Red");
+//        redLabel.setForeground ( Color.blue );
+//
+//        JLabel greenLabel = new JLabel ("Green");
+//        greenLabel.setForeground ( Color.MAGENTA );
+//        greenLabel.setVerticalAlignment ( JLabel.TOP );
+//        greenLabel.setHorizontalAlignment ( JLabel.CENTER );
+//
+//
+//        //JPanel
+//        JPanel blackPanel = new JPanel ();
+//        blackPanel.setBackground ( Color.BLACK );
+//        blackPanel.setBounds ( 0,0,250,250 );
+//
+//        JPanel redPanel = new JPanel ();
+//        redPanel.setBackground ( Color.RED);
+//        redPanel.setBounds ( 250,0,250,250 );
+//
+//        JPanel greenPanel = new JPanel ();
+//        greenPanel.setBackground ( Color.GREEN);
+//        greenPanel.setBounds ( 0,250,500,250 );
+//        greenPanel.setLayout ( new BorderLayout () );
 
         //JFrame
-        JFrame frame = new JFrame ();
-        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
-        frame.setTitle ( "Java GUI Test" );
-        frame.setSize ( 600,600 );
-        frame.setLayout ( null );
-        frame.setVisible ( true );
+//        JFrame frame = new JFrame ();
+//        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+//        frame.setTitle ( "Java GUI Test" );
+//        frame.setSize ( 600,600 );
+//        frame.setLayout ( null );
+//        frame.setVisible ( true );
       //  frame.add(label);
 //        frame.pack ();
+//
+//        blackPanel.add ( blackLabel );
+//        redPanel.add ( redLabel );
+//        greenPanel.add ( greenLabel );
+//
+//        frame.add ( blackPanel );
+//        frame.add ( redPanel );
+//        frame.add ( greenPanel );
+   //     frame.revalidate ();
 
-        blackPanel.add ( blackLabel );
-        redPanel.add ( redLabel );
-        greenPanel.add ( greenLabel );
 
-        frame.add ( blackPanel );
-        frame.add ( redPanel );
-        frame.add ( greenPanel );
-        frame.revalidate ();
-
-
+    MyFrame myFrame = new MyFrame ();
 
     }
 }
--- src/com/careerdevs/MyFrame.java
+++ src/com/careerdevs/MyFrame.java
@@ -1,24 +1,36 @@
 package com.careerdevs;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
 
+    JButton button;
 
     MyFrame(){
+        Border border = BorderFactory.createLineBorder ( Color.LIGHT_GRAY,3 );
+
+        button = new JButton ();
+        button.setBounds ( 100,100,250,100 );
+        button.setText ( "Press Me I'm A Button" );
+        button.setFocusable ( false );
+        button.setBorder (border);
+        button.setBackground ( Color.black );
+        button.addActionListener ( e -> System.out.println ("Button Pressed") );
+        button.validate ();
 
         this.setTitle ( "Testing GUI" );
-
-        this.setSize ( 420,420 );
-
+        this.setSize ( 600,600 );
         this.setVisible ( true );
-
         this.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
-
-        this.setResizable ( false );
-
         this.getContentPane ().setBackground (new Color ( 100,221,106 ) );
-
+        this.setLayout ( null );
+        this.add (button);
+        //this.validate ();
     }
+
+
 }
